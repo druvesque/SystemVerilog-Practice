21db4b8ef224781a8b6e3afbef8bfaef66c4f674
druvesque <dhruvsachdeva6480@gmail.com>
2025-12-26 09:38:30 +0530

    q

--- fundamentals/c18_queue_example1.sv
+++ fundamentals/c18_queue_example1.sv
@@ -1,5 +1,5 @@
 module example18;
-    int j = 1, b[$] = {3, 4}, q[$] = {0, 2, 5}, c[] = {10, 15, 20};
+    int j = 1, b[$] = {3, 4}, q[$] = {0, 2, 5}, c[] = {10, 15, 20}, d[$];
     initial begin
         q.insert(1, j);
         // q.insert(2, b);  Can't insert a queue in another queue
@@ -16,5 +16,9 @@ module example18;
         $display("q: %p", q);
         j = q.pop_front;
         $display("j: %0d, q: %p", j, q);
+        d.insert(0, 3);
+        d.push_front(5);
+        d.push_back(9);
+        $display("d: %p", d);
     end
 endmodule
