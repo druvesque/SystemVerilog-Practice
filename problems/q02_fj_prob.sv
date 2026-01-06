8ad1e75a79c38dd7b25f5d2d5b297767d1fc03c4
druvesque <dhruvsachdeva6480@gmail.com>
2026-01-06 11:38:37 +0530

    com

--- problems/q02_fj_prob.sv
+++ problems/q02_fj_prob.sv
@@ -1,17 +1,17 @@
 module q2;
     initial begin
-        $display("Time: %g, starting of example, 1st display statement", $time);
-        #10 $display("Time: %g, 2nd display statement (outside fork-join block)", $time);
+        $display("Time: %g, starting of example, 1st display statement", $time);          // PARENT THREAD
+        #10 $display("Time: %g, 2nd display statement (outside fork-join block)", $time); // PARENT THREAD
         fork
-            $display("Time: %g, parallel start", $time);
-            #10 $display("Time: %g, parallel start after #10", $time);
-            #50 $display("Time: %g, parallel start after #50", $time);
+            $display("Time: %g, parallel start", $time);                                  // CHILD THREAD - PARALLEL
+            #10 $display("Time: %g, parallel start after #10", $time);                    // CHILD THREAD - PARALLEL
+            #50 $display("Time: %g, parallel start after #50", $time);                    // CHILD THREAD - PARALLEL
             begin
-                #30 $display("Time: %g, sequential after #30", $time);
-                #10 $display("Time: %g, sequential after #10", $time);
+                #30 $display("Time: %g, sequential after #30", $time);                    // CHILD THREAD - SEQUENTIAL
+                #10 $display("Time: %g, sequential after #10", $time);                    // CHILD THREAD - SEQUENTIAL
             end
         join
-        $display("Time: %g, after join", $time);
-        #80 $display("Time: %g, final after #80", $time);
+        $display("Time: %g, after join", $time);                                          // PARENT THREAD
+        #80 $display("Time: %g, final after #80", $time);                                 // PARENT THREAD
     end
 endmodule
