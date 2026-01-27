d1bc1d126a63ab72196ba34c5d86f4f0bd1168d3
druvesque <dhruvsachdeva6480@gmail.com>
2026-01-27 12:07:19 +0530

    parallel

--- old/some_notes.sv
+++ old/some_notes.sv
@@ -7,5 +7,17 @@
 // GUI
 // TCL
 // CORE
+//
+//
+// Parallel Blocks in Verilog 
+//
+// - fork...join/join_any/join_none
+// - always (always_ff, always_latch, always_comb)
+// - multiple initial blocks
+// - multiple parallel blocks
+// - module instantiation
+// - gate instantiation
+// - primitive instances
+// - specify blocks
 
 
